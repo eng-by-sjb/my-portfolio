84aa5f0dfdd925550603f9e3f1a330c37be4111c
Stephen Jersuit Benyah <stephen.benyah.dev@gmail.com>
2023-11-14 12:48:36 +0000

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,26 @@
+import { links } from "../data";
+
 const Navbar = () => {
   return (
-    <nav className="bg-sky-200">
-      <div className="mx-auto max-w-7xl px-6 py-4 flex flex-col sm:flex-row sm:items-center sm:justify-between">
-        <div className="logo">thing</div>
-        <ul className="flex flex-col sm:flex-row sm:gap-x-2">
-          <li>home</li>
-          <li>about</li>
-          <li>contact</li>
-        </ul>
+    <nav className="bg-sky-100">
+      <div className="mx-auto max-w-7xl px-16 py-4 flex flex-col text-center sm:flex-row sm:items-center sm:justify-between">
+        <h2 className="text-3xl">
+          FullStack
+          <span className="text-sky-600 font-semibold">Dev</span>
+        </h2>
+
+        <div className="mt-4 flex gap-x-3 justify-center sm:flex-row sm:mt-0">
+          {links.map(({ href, id, text }) => {
+            return (
+              <a
+                key={id}
+                className="capitalize tracking-wide transition duration-300 ease-in hover:scale-110 hover:text-sky-600"
+                href={href}>
+                {text}
+              </a>
+            );
+          })}
+        </div>
       </div>
     </nav>
   );
